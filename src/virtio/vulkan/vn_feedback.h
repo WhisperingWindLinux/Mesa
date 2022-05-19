7464a52e0b9b9960ce56aa364034c4d871cfea58
Yiwei Zhang <zzyiwei@chromium.org>
2022-05-19 06:36:03 +0000

    venus: add vn_feedback_pool backed by coherent buffer (part 1)
    Implemented:
    - vn_feedback_pool_init
    - vn_feedback_pool_fini
    
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>

---
+++ src/virtio/vulkan/vn_feedback.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef VN_FEEDBACK_H
+#define VN_FEEDBACK_H
+
+#include "vn_common.h"
+
+struct vn_feedback_pool {
+   struct vn_device *device;
+   const VkAllocationCallbacks *alloc;
+
+   uint32_t size;
+   uint32_t used;
+
+   /* first entry is the active feedback buffer */
+   struct list_head feedback_buffers;
+};
+
+VkResult
+vn_feedback_pool_init(struct vn_device *dev,
+                      struct vn_feedback_pool *pool,
+                      uint32_t size,
+                      const VkAllocationCallbacks *alloc);
+
+void
+vn_feedback_pool_fini(struct vn_feedback_pool *pool);
+
+#endif /* VN_FEEDBACK_H */

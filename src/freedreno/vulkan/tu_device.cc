f3ca3fd2a95b1f09a58f40d15e6dff66feb66bc6
Connor Abbott <cwabbott0@gmail.com>
2024-03-05 12:02:46 -0500

    tu: Expose VK_KHR_dynamic_rendering_local_read

--- src/freedreno/vulkan/tu_device.cc
+++ src/freedreno/vulkan/tu_device.cc
@@ -156,6 +156,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .KHR_draw_indirect_count = true,
       .KHR_driver_properties = true,
       .KHR_dynamic_rendering = true,
+      .KHR_dynamic_rendering_local_read = true,
       .KHR_external_fence = true,
       .KHR_external_fence_fd = true,
       .KHR_external_memory = true,
@@ -451,6 +452,9 @@ tu_get_features(struct tu_physical_device *pdevice,
    features->dynamicRendering                    = true;
    features->shaderIntegerDotProduct             = true;
    features->maintenance4                        = true;
+
+   /* VK_KHR_dynamic_rendering_local_read */
+   features->dynamicRenderingLocalRead = true;
 
    /* VK_KHR_index_type_uint8 */
    features->indexTypeUint8 = true;

91b816d2f1d79678f170f4c23a1fd32c79c9b856
José Roberto de Souza <jose.souza@intel.com>
2024-08-22 07:38:33 -0700

    anv: Set all async compute registers in STATE_COMPUTE_MODE
    Setting the missing registers to specification recommended values that
    is also the default value, so it is not expected any changes in
    behavior or performance here.
    
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>

--- src/intel/vulkan/genX_init_state.c
+++ src/intel/vulkan/genX_init_state.c
@@ -740,8 +740,12 @@ init_compute_queue_state(struct anv_queue *queue)
 
    anv_batch_emit(batch, GENX(STATE_COMPUTE_MODE), cm) {
 #if GFX_VER < 20
-      cm.PixelAsyncComputeThreadLimit = 4;
+      cm.PixelAsyncComputeThreadLimit = PACTL_Max24;
+      cm.ZPassAsyncComputeThreadLimit = ZPACTL_Max60;
+      cm.ZAsyncThrottlesettings = ZATS_DefertoPixelAsyncComputeThreadLimit;
       cm.PixelAsyncComputeThreadLimitMask = 0x7;
+      cm.ZPassAsyncComputeThreadLimitMask = 0x7;
+      cm.ZAsyncThrottlesettingsMask = 0x3;
 #endif
    }
 #endif

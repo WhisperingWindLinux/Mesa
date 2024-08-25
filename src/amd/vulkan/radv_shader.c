8e9339afe40a225147a50a0fb6a94e3bb646d891
Konstantin Seurer <konstantin.seurer@gmail.com>
2024-08-25 13:57:16 +0200

    radv: Store debug info inside radv_shader

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -2199,6 +2199,7 @@ struct radv_shader_binary_data {
    void *code;
    void *ir;
    void *disasm;
+   void *debug_info;
 };
 
 static struct radv_shader_binary_data
@@ -2219,6 +2220,9 @@ radv_shader_binary_get_data(struct radv_shader_binary_legacy *binary)
 
    data.disasm = binary->data + offset;
    offset += binary->disasm_size;
+
+   data.debug_info = binary->data + offset;
+   offset += binary->debug_info_size;
 
    return data;
 }
@@ -2767,8 +2771,12 @@ radv_aco_build_shader_binary(void **bin, const struct ac_shader_config *config, 
                              const struct aco_debug_info *debug_info, unsigned debug_info_count)
 {
    struct radv_shader_binary **binary = (struct radv_shader_binary **)bin;
+
+   uint32_t debug_info_size = debug_info_count * sizeof(struct aco_debug_info);
+
    size_t size = llvm_ir_size;
 
+   size += debug_info_size;
    size += disasm_size;
    size += stats_size;
 
@@ -2787,6 +2795,7 @@ radv_aco_build_shader_binary(void **bin, const struct ac_shader_config *config, 
    legacy_binary->code_size = code_dw * sizeof(uint32_t);
    legacy_binary->ir_size = llvm_ir_size;
    legacy_binary->disasm_size = disasm_size;
+   legacy_binary->debug_info_size = debug_info_size;
 
    struct radv_shader_binary_data data = radv_shader_binary_get_data(legacy_binary);
 
@@ -2800,6 +2809,9 @@ radv_aco_build_shader_binary(void **bin, const struct ac_shader_config *config, 
 
    if (disasm_size)
       memcpy(data.disasm, disasm_str, disasm_size);
+
+   if (debug_info_size)
+      memcpy(data.debug_info, debug_info, debug_info_size);
 
    *binary = (struct radv_shader_binary *)legacy_binary;
 }
@@ -2877,6 +2889,10 @@ radv_capture_shader_executable_info(struct radv_device *device, struct radv_shad
 
       shader->ir_string = bin->ir_size ? strdup(data.ir) : NULL;
       shader->disasm_string = bin->disasm_size ? strdup(data.disasm) : NULL;
+
+      shader->debug_info = malloc(bin->debug_info_size);
+      memcpy(shader->debug_info, data.debug_info, bin->debug_info_size);
+      shader->debug_info_count = bin->debug_info_size / sizeof(struct aco_debug_info);
    }
 }
 

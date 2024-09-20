be98b4f006b608b0a7c18a4e92ccad89a8b52ee9
Antonio Ospite <antonio.ospite@collabora.com>
2024-09-20 13:42:44 +0200

    fixup for ci/android: get prebuilt cuttlefish images from the net
    Mention that the change is to get CUSTOM images as they were fetched
    form the network even before.

--- .gitlab-ci/container/debian/x86_64_test-android.sh
+++ .gitlab-ci/container/debian/x86_64_test-android.sh
@@ -63,20 +63,22 @@ EXTRA_CMAKE_ARGS="-DDEQP_TARGET_TOOLCHAIN=ndk-modern -DANDROID_NDK_PATH=/$ndk -D
 
 ############### Downloading Cuttlefish resources ...
 
-CUTTLEFISH_VERSION=mesa+llvmpipe-20240820
+CUTTLEFISH_PROJECT_PATH=ao2/aosp-manifest
+CUTTLEFISH_BUILD_VERSION_TAGS=mesa-llvmpipe
+CUTTLEFISH_BUILD_NUMBER=20240902.001
 
 mkdir /cuttlefish
 pushd /cuttlefish
 
 curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
-  -o aosp_cf_x86_64_phone-img-$CUTTLEFISH_VERSION.zip https://people.collabora.com/~ao2/aosp_cf_x86_64_phone-img-$CUTTLEFISH_VERSION.zip
+  -o aosp_cf_x86_64_phone-img-$CUTTLEFISH_BUILD_NUMBER.zip "https://${S3_HOST}/${S3_ANDROID_BUCKET}/${CUTTLEFISH_PROJECT_PATH}/aosp-${CUTTLEFISH_BUILD_VERSION_TAGS}.${CUTTLEFISH_BUILD_NUMBER}/aosp_cf_x86_64_phone-img-$CUTTLEFISH_BUILD_NUMBER.zip"
 
-unzip aosp_cf_x86_64_phone-img-$CUTTLEFISH_VERSION.zip
-rm aosp_cf_x86_64_phone-img-$CUTTLEFISH_VERSION.zip
+unzip aosp_cf_x86_64_phone-img-$CUTTLEFISH_BUILD_NUMBER.zip
+rm aosp_cf_x86_64_phone-img-$CUTTLEFISH_BUILD_NUMBER.zip
 ls -lhS ./*
 
 curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \
-  -o cvd-host_package.tar.gz https://people.collabora.com/~ao2/cvd-host_package-$CUTTLEFISH_VERSION.tar.gz
+  -o cvd-host_package.tar.gz "https://${S3_HOST}/${S3_ANDROID_BUCKET}/${CUTTLEFISH_PROJECT_PATH}/aosp-${CUTTLEFISH_BUILD_VERSION_TAGS}.${CUTTLEFISH_BUILD_NUMBER}/cvd-host_package.tar.gz"
 tar -xzvf cvd-host_package.tar.gz
 rm cvd-host_package.tar.gz
 
